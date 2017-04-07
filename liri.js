d4c6a113fd15e5696e9134977dbabd8ceb23cd6e
DKMitt <dkmitt@sbcglobal.net>
2017-04-07 16:41:56 -0500

    Twitter working - cleaned up twitter section

--- liri.js
+++ liri.js
@@ -15,49 +15,67 @@ inquirer.prompt([
   	// console.log(JSON.stringify(confData, null, 2));
   	
     if (confData.selections == "my-tweets") {
-  		console.log('working... my-tweets');
+      console.log('')
+  		console.log('You selected... my-tweets');
   		// all of the code for my-tweets goes here
+        
         var client = new Twitter(keys.twitterKeys);
          
         var params = {screen_name: 'MittTechnology', count: 20};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
           if (!error) {
-            // console.log(tweets);
+             // console.log(tweets);
             for (var i = 0; i < tweets.length; i++) {
-            	console.log(tweets[i].text)
-            	console.log(i+1)
-            	console.log(tweets[i].created_at)
+              console.log('')
+              console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            	console.log('Tweet: ' + tweets[i].text)
+              console.log('')
+            	console.log("Tweet Number: " + (i+1))
+              console.log('')
+            	console.log('Created: ' + tweets[i].created_at)
+              console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+              console.log('')
             }
           }
         });
 
 
   	} else if (confData.selections == "spotify-this-song") {
-  		console.log('working... spotify-this-song');
+      console.log('')
+  		console.log('You selected... spotify-this-song - Sorry but this selection is not active at this time');
   		// all of the spotify code will go here
 
 
 
   	} else if (confData.selections == "movie-this") {
-  		console.log('working... movie-this');
+      console.log('')
+  		console.log('You selected... movie-this');
   		// all of the movie-this code goes here
 	
 			var query = "http://www.omdbapi.com/?t=cinderella";
 			
 			request(query, function(error, response, body) {
 				if(!error && response) {
-					console.log("Release Year: " +JSON.parse(body)["Year"]);
 
+          console.log('')
+          console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+					console.log("Release Year: " +JSON.parse(body)["Year"]);
+          
+
+          console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+          console.log('')
 				}
 			});
 
 
   	} else if (confData.selections == "do-what-it-says") {
-  		console.log('working... do-what-it-says');
+      console.log('')
+  		console.log('You selected... do-what-it-says - Sorry but this selection is not active at this time');
   		// all of the do-what-it-says code goes here
 
 
   	} else {
-  		console.log('i dont understand...');
+      console.log('')
+  		console.log('i dont understand... you gave me bad input');
   	}
   });
